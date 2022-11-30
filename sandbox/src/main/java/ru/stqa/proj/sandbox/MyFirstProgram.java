5af14e56678490384292cd699505b6947d847da4
Dmitry Plotnikov <103034855+oOBUGagaOo@users.noreply.github.com>
2022-11-30 15:59:10 +0300

    Update MyFirstProgram.java

--- sandbox/src/main/java/ru/stqa/proj/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/proj/sandbox/MyFirstProgram.java
@@ -3,7 +3,11 @@ package ru.stqa.proj.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
+    String somebody = "world";
     System.out.println("Hello, world!");
-  }
 
+    double l = 8.0;
+    double s = l * l;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + s);
+  }
 }
